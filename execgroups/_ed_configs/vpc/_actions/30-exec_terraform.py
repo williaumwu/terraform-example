07b5438d4b602ee110844a9992ac6ba4b0058e12
Gary <gary@elasticdev.io>
2020-07-24 16:25:35 +0800

    added vpc for gcloud

---
+++ execgroups/_ed_configs/vpc/_actions/30-exec_terraform.py
@@ -0,0 +1,15 @@
+def default():
+    
+    task = {}
+    env_vars = []
+    shelloutconfigs = []
+
+    # This can be overided by env_vars in the resource add
+    shelloutconfigs.append('elasticdev:::terraform::resource_wrapper')
+
+    task['method'] = 'shelloutconfig'
+    task['metadata'] = {'env_vars': env_vars,
+                        'shelloutconfigs': shelloutconfigs
+                        }
+
+    return task
